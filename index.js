9816856b5299a7c4cef6d69dfed948cca3303c5d
Jacqueline Rivera <Jacqueline8996@gmail.com>
2021-02-23 16:34:35 -0500

    added more into inquiror

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 //Importing data and programs from  othere folders
 const inquirer = require("inquirer");
-const manager = require("./lib/Manager");
-const engineer = require("./lib/Engineer");
-const intern = require("./lib/Intern");
-const buildTeam = require("./src/buildTeam");
 
 
+
+//ask the user in nodes what question,and then put the information into a read me file
+const askEmployee = () =>
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is the Title of your project?',
+            name: 'title'
+        },
+       
+    ]);
