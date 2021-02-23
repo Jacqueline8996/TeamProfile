b77125cf710116c054c6032fc0af7ef31bc84263
Jacqueline Rivera <Jacqueline8996@gmail.com>
2021-02-23 15:50:38 -0500

    added more files

---
+++ index.js
@@ -0,0 +1,8 @@
+//Importing data and programs from  othere folders
+const inquirer = require("inquirer");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const buildTeam = require("./src/buildTeam");
+
+
