2248ce3681db5509c001288650a96b03ea03a6e5
Jacqueline Rivera <Jacqueline8996@gmail.com>
2021-02-28 18:42:17 -0500

    added constructors to Employee/engineer/Intern

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 //Importing data and programs from  othere folders
 const inquirer = require("inquirer");
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager.js");
+const Engineer = require("./lib/Engineer.js");
+const Intern = require("./lib/Intern.js");
 
 const jobRank = ['Engineer', 'Intern','Manager']
 // function Person(employeeName,employeeID,employeeEmail,rank){
